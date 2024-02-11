f9087db63bcc78ad3c824afa9d1b8873c842ec96
ajik <ajikusuma@gmail.com>
2024-02-12 00:13:10 +0700

    latest commit

---
+++ assets/js/send-email.js
@@ -0,0 +1,18 @@
+(function(){
+    emailjs.init({
+        publicKey: "NcxFYKoP-abKw6hjQ",
+    });
+})();
+
+function sendMail() {
+    let params = {
+        from_name : document.getElementById("smName").value,
+        email : document.getElementById("smEmail").value,
+        subject : document.getElementById("smSubject").value,
+        message : document.getElementById("smMessage").value
+    }
+
+    emailjs.send("service_01", "template_01", params).then(
+        alert("Your message has been sent, Thank You!")
+    )
+}
